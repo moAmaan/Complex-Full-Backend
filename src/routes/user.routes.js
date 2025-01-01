90be5ca8ac225479a8109fb7e1c36c7db9744ef7
Amaan <itsmak786@gmail.com>
2025-01-01 21:31:57 +0530

    DB aggregation pipeline added for channel details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import {
+  changePassword,
+  getCurrentUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -24,10 +28,28 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
 
 // secured routes
 
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/change-password").post(verifyJWT, changePassword);
+router.route("/get-user").post(verifyJWT, getCurrentUser);
+router.route("/update-avatar").post(
+  upload.single({
+    name: "avatar",
+    maxCount: 1,
+  }),
+  verifyJWT,
+  updateAvatar
+);
+router.route("/update-coverImage").post(
+  upload.single({
+    name: "coverImage",
+    maxCount: 1,
+  }),
+  verifyJWT,
+  updateUserCoverImage
+);
 
 export default router;
